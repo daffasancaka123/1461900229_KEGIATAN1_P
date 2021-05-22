d1d572ebe71eaaa958c2b6ed6ef29b4e1dbc0f37
Daffa Sancaka <sancakadaffa@gmail.com>
2021-05-22 11:32:07 +0700

    home

---
+++ resources/views/artikel.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Artikel</title>
+</head>
+<body
+><p>Artikel</p>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <p>Home</p>
+</body>
+<a href="{{url('/artikel')}}">Pindah Halaman</a>
+</html>
